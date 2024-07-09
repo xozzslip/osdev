b9ed8dd917126aeda2d39d2d1e4b178d30916dd8
khasan <xozzslip@gmail.com>
2024-07-09 13:36:20 +0300

    max size of kernel now in 254KiB

--- kernel/src/boot/boot.asm
+++ kernel/src/boot/boot.asm
@@ -1,18 +1,15 @@
 [org 0x7c00]
 [bits 16]
     ; reading from disk
-    mov bx, 0x1000 ; memory address where data will be loaded to
+    mov bx, 0x7E00 ; memory address where data will be loaded to
     mov ah, 2 ; int 0x13 command
-    mov al, 48 ; number of sectors to read (read 48 sectors, 24 kilobytes)
+    mov al, 127 ; number of sectors to read (read 127 sectors (254 KiB))
     mov cl, 2 ; sector number  (1-17 dec.) enumerated from 1!
     mov ch, 0 ; track number is zero (hopefully that track has a few sectors)
     ; dl should be already set by BIOS, it is a number of disk
     mov dh, 0
     int 0x13
     cmp ah, 0 ; check for errros
-
-    ; mov bx, 0x1000
-    ; call print16_str
 
     ; switching to protected mode
     cli
@@ -21,9 +18,6 @@
     or eax, 1
     mov cr0, eax
     jmp segment_code:protected_mode_entry_point
-
-%include "src/boot/print16.asm"
-%include "src/boot/gdt.asm"
 
 [bits 32]
 protected_mode_entry_point:
@@ -37,10 +31,13 @@ protected_mode_entry_point:
     ; print via memory mapped VGA
     mov ebx, 0xb8000 + 2 * (14 * 80 + 20)
     mov eax, 0
-    mov al, 'X' ; write 'X' in the middle of screen
+    mov al, 'C' ; write 'X' in the middle of screen
     mov ah, 0x0f ; white on black
     mov [ds:ebx], eax
-    jmp 0x1000
+    jmp 0x7E00
+
+%include "src/boot/print16.asm"
+%include "src/boot/gdt.asm"
 
 times 510 - ($-$$) db 0
 dw 0xaa55
