0c1bc4fb96315fbfa6e04af71e59d5cc9e8c85ba
khasan <xozzslip@gmail.com>
2024-07-21 23:52:47 +0300

    variant number 10000 of how to make syscalls

--- kernel/src/userspace/screensaver.c
+++ kernel/src/userspace/screensaver.c
@@ -10,54 +10,41 @@
 #include <stddef.h>
 #include <stdint.h>
 
+uint32_t syscall(
+    uint32_t syscall_no,
+    uint32_t param1,
+    uint32_t param2,
+    uint32_t param3)
+{
+    uint32_t ret; // To store the return value of the syscall
+    __asm__ volatile(
+        "int $0x80" // Interrupt instruction to invoke the system call
+        : "=a"(ret) // Output: eax will hold the return value after syscall
+        : "a"(syscall_no), // Input: eax should hold the syscall number
+        "b"(param1), // ebx should hold the first argument
+        "c"(param2), // ecx should hold the second argument
+        "d"(param3) // edx should hold the third argument
+        : "memory", "cc" // Tell the compiler to expect changes in memory and condition codes
+    );
+    return ret;
+}
+
 typedef enum {
-    KR_OPEN,
-    KR_READ,
+    KR_RECV_NONBLOCK,
     KR_WRITE,
     KR_SEEK,
     KR_ALLOCATE_MEMORY,
     KR_MAP_MEMORY,
     KR_WAIT,
-} KR_Type;
+} KernelRequestType;
 
-typedef struct {
-    KR_Type type;
-    union
-    {
-        KR_OPEN_Request open_request;
-        KR_OPEN_Response open_response;
-    } data;
-
-} KR;
-
-typedef struct {
-    const char *path;
-} KR_OPEN_Request;
-
-typedef struct {
-    uint32_t error;
-} KR_OPEN_Response;
-
-
-void syscall(KR* request)
+int32_t recv_nonblock(const char* path, void* buf, size_t size)
 {
-    __asm__ volatile(
-    "int $0x80"
-    : // no return value
-    : "a"((void*)request), // eax
-    "b"(1) // ebx
-    // Tell the compiler to expect changes in memory and condition codes
-    : "memory", "cc");
-}
-
-
-uint32_t open(KR_OPEN_Request req)
-{
-    KR request = {0};
-    request.type = KR_OPEN;
-    request.data.open_request = req;
-    syscall(&request);
-    return request.data.open_response;
+    return syscall(
+        KR_RECV_NONBLOCK,
+        (uint32_t)path,
+        (uint32_t)buf,
+        (uint32_t)size);
 }
 
 WindowBuffer window;
@@ -68,10 +55,20 @@ int pmain()
     uint8_t* first_ptr = 0;
     uint8_t* last_ptr = 0;
 
-
+    int32_t window_fd = open("/dev/window");
+    if (window_fd <= 0) {
+        return -1;
+    }
 
     for (;;) {
-
+        KR_WindowResizedEvents events = poll_window_resized_events();
+        if (events.count > 0) {
+            KR_WindowResizedEvent resize = events.events[events.count - 1];
+            window.width = resize.width;
+            window.height = resize.height;
+            window.buffer = (uint8_t*)malloc(window.width * window.height * 2);
+            mmap_window(window)
+        }
 
         for (int row = 0; row < window.height; row++) {
             for (int col = 0; col < window.width; col++) {
